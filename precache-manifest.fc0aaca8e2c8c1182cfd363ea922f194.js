907ddc0a7ad5d296bfde7921008c4ec06228fd8d
EVERTZ_MICROSYS\kng <kng@evertz.com>
2019-06-19 16:07:10 -0400

    Updates

---
+++ precache-manifest.fc0aaca8e2c8c1182cfd363ea922f194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef832336362db978ccf81c268392f6d",
+    "url": "/slotmachine/index.html"
+  },
+  {
+    "revision": "b2d832b95fc1b10bdc16",
+    "url": "/slotmachine/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d8578d87d336067f516b",
+    "url": "/slotmachine/static/css/main.9288ccd4.chunk.css"
+  },
+  {
+    "revision": "b2d832b95fc1b10bdc16",
+    "url": "/slotmachine/static/js/2.d140d620.chunk.js"
+  },
+  {
+    "revision": "d8578d87d336067f516b",
+    "url": "/slotmachine/static/js/main.5d9d80bb.chunk.js"
+  },
+  {
+    "revision": "4dbb6ac398f214fd46a5",
+    "url": "/slotmachine/static/js/runtime~main.7aab269d.js"
+  }
+]);
