ca62bbeecf4d92f73cdc35f4310694d048c2c71b
Kelly Ng <kelly.ng1@outlook.com>
2021-01-02 13:14:25 -0500

    Updates

---
+++ precache-manifest.6ceb3a83490485eb76d4ace2767b1bb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a815e03b19a97bf66921cbfb736b2ca",
+    "url": "/slotmachine/index.html"
+  },
+  {
+    "revision": "e77f7c4e9036cde5d484",
+    "url": "/slotmachine/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "97be57ac924166938813",
+    "url": "/slotmachine/static/css/main.da463f53.chunk.css"
+  },
+  {
+    "revision": "e77f7c4e9036cde5d484",
+    "url": "/slotmachine/static/js/2.f9db5f14.chunk.js"
+  },
+  {
+    "revision": "97be57ac924166938813",
+    "url": "/slotmachine/static/js/main.41fa68f8.chunk.js"
+  },
+  {
+    "revision": "4dbb6ac398f214fd46a5",
+    "url": "/slotmachine/static/js/runtime~main.7aab269d.js"
+  },
+  {
+    "revision": "b0db68f3bc234fa7810842f4a70bb6a8",
+    "url": "/slotmachine/static/media/jellyfish.b0db68f3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/slotmachine/precache-manifest.fc0aaca8e2c8c1182cfd363ea922f194.js"
+  "/slotmachine/precache-manifest.6ceb3a83490485eb76d4ace2767b1bb0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slotmachine/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
